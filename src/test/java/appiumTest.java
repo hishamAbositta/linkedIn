fd61bdce3a214f7133934165f7ecaaa031fbe9c2
hishamAbositta <hishamabositta@gmail.com>
2021-03-09 00:29:00 +0200

    add login

--- src/test/java/appiumTest.java
+++ src/test/java/appiumTest.java
@@ -1,5 +1,9 @@
 import io.appium.java_client.*;
+import io.appium.java_client.android.AndroidDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -10,6 +14,7 @@ import java.util.concurrent.TimeUnit;
 
 public class appiumTest {
     static AppiumDriver<MobileElement> driver;
+
     @BeforeClass
     public void setup()  throws MalformedURLException{
         DesiredCapabilities cap = new DesiredCapabilities();
@@ -21,12 +26,17 @@ public class appiumTest {
         cap.setCapability("appActivity","com.linkedin.android.authenticator.LaunchActivity");
         URL url = new URL("http://127.0.0.1:4723/wd/hub");
         driver = new AppiumDriver(url,cap);
-        driver.manage().timeouts().implicitlyWait(1, TimeUnit.SECONDS);;
     }
     @Test(priority = 1)
     public static void openLinkedIn()  {
-        MobileElement signIn = driver.findElement(By.xpath("//android.widget.Button[@text='Sign in']"));
-        signIn.click();
+        MobileElement signin = driver.findElement(By.id("com.linkedin.android:id/growth_prereg_fragment_login_button"));
+        signin.click();
+        MobileElement Email = driver.findElement(By.id("com.linkedin.android:id/growth_prereg_fragment_login_Email"));
+        Email.sendKeys("test1212");
+        MobileElement pass = driver.findElement(By.id("com.linkedin.android:id/growth_prereg_fragment_login_pass"));
+        pass.sendKeys("test12121313");
+        MobileElement continueBtn = driver.findElement(By.id("com.linkedin.android:id/growth_prereg_fragment_login_continue"));
+        continueBtn.click();
 
 
 
